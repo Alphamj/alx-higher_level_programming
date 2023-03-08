51c230f53865898be73cb84198d8f09435257255
Alphamj <mjack306@gmail.com>
2023-03-08 13:47:59 -0800

    Task 6.1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if j != i:
-            print(f"{i}{j}, ", end="")
+for num in range(0, 10):
+    for num2 in range(0, 10):
+        if num is 8 and num2 is 9:
+            print("{:d}{:d}".format(num, num2))
+        elif num < num2:
+            print("{:d}{:d}".format(num, num2), end=", ")
