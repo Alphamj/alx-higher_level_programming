c482c324292f4c0c2e6be69ad38e23f037e503f0
Alphamj <mjack306@gmail.com>
2023-03-08 13:55:10 -0800

    Task 11.5

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    val = a
-    for i in range(b):
-        if b < 0:
-            val = b / a
-            return val
-        elif b > 0:
-            val = val * a
-            return val
-        else:
-            val = 1
-            return val
+    return (a ** b)
